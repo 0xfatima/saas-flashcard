1faa3c996b2532d068705705f5c31e9b50dff694
fatima22234 <emaaaa37@gmail.com>
2024-08-19 00:41:58 +0500

    security

--- firebase.js
+++ firebase.js
@@ -8,12 +8,12 @@ import { getFirestore} from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAICoTK_1B6H270kseTDLftanhr4MX7Gx4",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "saas-flashcardsa-ai.firebaseapp.com",
   projectId: "saas-flashcardsa-ai",
   storageBucket: "saas-flashcardsa-ai.appspot.com",
-  messagingSenderId: "930993377632",
-  appId: "1:930993377632:web:6e32c6fc24d30423c54e05",
+  messagingSenderId:process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId:process.env.FIREBASE_APPID,
   measurementId: "G-G23C57MWSV"
 };
 
